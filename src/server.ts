1b6edf91a077e7dc7ce254794bd1bd493a0975ca
gilert <crankyfox@protonmail.com>
2024-10-27 11:49:28 +0100

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import cors from 'cors';
 import routes from './routes';
 import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
 app.use(bodyParser.json());
+const corsOptions = {
+    origin: process.env.ALLOWED_URL, // or use an array for multiple origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // allowed HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // allowed headers
+    // credentials: true, // if credentials (cookies, authorization headers) are needed
+  };
+app.use(cors(corsOptions));
 
 app.use('/api', routes);
 
