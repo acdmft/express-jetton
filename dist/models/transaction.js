43301378687da69c8d024a05e42527e1c5eab664
gilert <crankyfox@protonmail.com>
2024-10-27 11:19:25 +0100

    add dist in gitignore

--- dist/models/transaction.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// export interface Transaction {
-//     id: number;
-//     hash: string;
-//     created_ad: Date;
-//     comment: string;
-// }
-Object.defineProperty(exports, "__esModule", { value: true });
--- dist/routes.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const txController_1 = require("./controllers/txController");
-const txVerifMiddleware_1 = require("./middleware/txVerifMiddleware");
-const router = (0, express_1.Router)();
-router.post('/transaction', txVerifMiddleware_1.verifyTx, txController_1.processTx);
-router.get('/echo', txController_1.echo);
-exports.default = router;
