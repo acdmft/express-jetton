6db6b2421c15d2942f761ac581ab1347914be93d
gilert <crankyfox@protonmail.com>
2024-11-11 18:57:50 +0100

    add check for script running in cli

--- src/config/database.ts
+++ src/config/database.ts
@@ -1,25 +1,27 @@
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
+import { Pool } from "pg";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const conn = process.env.DATABASE_URL!;
 const pool = new Pool({
-    connectionString: conn
-})
+  connectionString: conn,
+});
 
 const testConnection = async () => {
-    try {
-        const client = await pool.connect();
-        console.log("Connected to PostgreSQL database successfully!");
-        client.release();
-    } catch (err) {
-        console.error("Failed to connect to PostgreSQL database:", err);
-    } finally {
-        pool.end();
-    }
+  try {
+    const client = await pool.connect();
+    console.log("Connected to PostgreSQL database successfully!");
+    client.release();
+  } catch (err) {
+    console.error("Failed to connect to PostgreSQL database:", err);
+  } finally {
+    pool.end();
+  }
 };
 
-// testConnection();
+if (require.main == module) {
+  testConnection();
+}
 
 export default pool;
