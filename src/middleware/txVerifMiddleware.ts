65bdd3eedcff82905bc985f8e1dab6bd0dee7bc3
gilert <crankyfox@protonmail.com>
2024-12-08 12:27:14 +0100

    fix verifyTx middleware

--- src/middleware/txVerifMiddleware.ts
+++ src/middleware/txVerifMiddleware.ts
@@ -4,8 +4,8 @@ export async function verifyTx(req: Request, res: Response, next: NextFunction) 
 
     const { msgHash, txHash, comment } = req.body;
     // console.log('msgHash , txHash, comment ', msgHash, txHash, comment);
-    if (msgHash.length !== 44 || txHash.length !== 64 || comment.length > 328) {
-        res.status(500).json({ error: 'Malformed transaction. Maximum number of letters for comment - 328' })
+    if (msgHash.length !== 44 || txHash.length !== 44 || comment.length > 328) {
+        return res.status(500).json({ error: 'Malformed transaction. Maximum number of letters for comment - 328' })
     }
     next();
 }
